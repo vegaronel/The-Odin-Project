3b61a9cba65df4ee41847a3bf4a20814b3df0290
vegaronel <ronelvega31@gmail.com>
2025-03-24 12:59:27 +0800

    add ui

--- JAVASCRIPT/Problem Solving - Understanding Errors/1.2 Rock Paper Scissor/script.js
+++ JAVASCRIPT/Problem Solving - Understanding Errors/1.2 Rock Paper Scissor/script.js
@@ -1,8 +1,14 @@
+const buttons = document.querySelectorAll("button");
+const resultView = document.getElementById("result");
+const currentScore = document.getElementById("score");
+
 let computerScore = 0;
 let humanScore = 0;
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumber);
+
   if (randomNumber === 1) {
     return "rock";
   } else if (randomNumber === 2) {
@@ -26,7 +32,8 @@ function getHumanChoice() {
 }
 
 function playRound(computerChoice, humanChoice) {
-  if (computerChoice === "rock" && humanChoice === "paper") {
+  console.log(computerChoice, humanChoice);
+  if (humanChoice === "rock" && computerChoice === "paper") {
     computerScore++;
     console.log("Human: " + humanChoice + " Computer: " + computerChoice);
     return "Computer Wins";
@@ -48,26 +55,22 @@ function playRound(computerChoice, humanChoice) {
   }
 }
 
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    const computerChoice = getComputerChoice();
-    const humanChoice = getHumanChoice();
-    const result = playRound(computerChoice, humanChoice);
-    console.log(
-      result +
-        "\nScore: Human: " +
-        humanScore +
-        " Computer: " +
-        computerScore +
-        "\n   "
-    );
-  }
-
-  if (humanScore > computerScore) {
-    console.log("The winner is: Human");
-  } else {
-    console.log("The winner is: Computer");
-  }
+function playGame(human) {
+  const computerChoice = getComputerChoice();
+  const result = playRound(computerChoice, human);
+  resultView.textContent = result;
+  currentScore.textContent =
+    "Human: " + humanScore + " Computer: " + computerScore;
 }
 
-playGame();
+buttons.forEach((button) => {
+  button.addEventListener("click", function (event) {
+    playGame(event.target.innerHTML.toLocaleLowerCase());
+  });
+});
+
+if (humanScore > computerScore) {
+  console.log("The winner is: Human");
+} else {
+  console.log("The winner is: Computer");
+}
