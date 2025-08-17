ea14c7bd54c905c90bc1b8bdd88f58c8225715f5
vegaronel <ronelvega31@gmail.com>
2025-08-17 12:33:11 +0800

    final

--- CAPSTONES/004-Books/index.js
+++ CAPSTONES/004-Books/index.js
@@ -5,53 +5,85 @@ const showModal = document.getElementById("showModal");
 const newBookModal = document.querySelector(".action-container");
 const closeButton = document.getElementById("closeButton");
 const errorTitle = document.getElementById("error-title");
-const imageLink = document.getElementById("image-link");
+const imageFile = document.getElementById("imageFile");
+const fileChosen = document.getElementById("fileChosen");
+const description = document.getElementById("description");
 
-const myLibrary = [];
-
-function Books(id, title, img, finished) {
+const myLibrary = [
+  {
+    id: crypto.randomUUID(),
+    title: "To Kill a Mockingbird",
+    img: "https://static1.squarespace.com/static/5fa57aacf5b0a90a76b0d7cc/t/6242b3435418df43b4c7d73c/1648538435933/Harper+Lee+-+To+Kill+A+Mockingbird.jpg?format=1500w",
+    finished: "Read",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+  {
+    id: crypto.randomUUID(),
+    title: "The Great Gatsby",
+    img: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1650033243i/41733839.jpg",
+    finished: "Not Read",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+  },
+];
+function Books(id, title, img, finished, description) {
   this.id = id;
   this.title = title;
   this.img = img;
   this.finished = finished;
+  this.description = description;
 }
 
 function addBookToLibrary() {
+  const file = imageFile.files[0];
+
   const readStatus = document.querySelector('input[name="finished"]:checked');
   if (!title.value) {
     return (errorTitle.style.display = "block");
   }
+  // if (!file) {
+  //   return console.log("error uploading file");
+  // }
   const newBook = new Books(
     crypto.randomUUID(),
     title.value,
-    imageLink.value,
-    readStatus.value
+    file
+      ? URL.createObjectURL(file)
+      : "https://avatars.githubusercontent.com/u/4441966?s=280&v=4",
+    readStatus.value,
+    description.value
   );
   myLibrary.push(newBook);
   showBooks();
   title.value = "";
-  imageLink.value = "";
+  imageFile.value = "";
   errorTitle.style.display = "none";
   newBookModal.style.display = "none";
+  fileChosen.style.display = "none";
+  description.value = "";
 }
 
 function showBooks() {
-  const div = document.createElement("div");
-  const img = document.createElement("img");
-  const title = document.createElement("h2");
-  const isRead = document.createElement("small");
-
   myLibrary.forEach((item) => {
+    const div = document.createElement("div");
+    const img = document.createElement("img");
+    const title = document.createElement("h2");
+    const isRead = document.createElement("small");
+    const description = document.createElement("p");
+    console.log(item);
     div.classList.add("book-item");
     img.classList.add("book-image");
-    isRead.textContent = item.finished;
-    img.src = item.img
-      ? item.img
-      : "https://media.istockphoto.com/id/162833245/photo/blank-book.jpg?s=612x612&w=0&k=20&c=7e-A5MDfHe0hGY2tO3WI_MgrGQ5HWgigMEVkKk1bR14=";
-    title.textContent = item.title;
+    isRead.textContent = "Status: " + item.finished;
+    img.src = item.img;
+    title.innerHTML = item.title;
+    description.textContent = item.description;
+    description.classList.add("description");
+
     div.append(img);
     div.append(title);
     div.append(isRead);
+    div.append(description);
     bookList.append(div);
   });
 }
@@ -64,4 +96,12 @@ showModal.addEventListener("click", function () {
 });
 closeButton.addEventListener("click", function () {
   newBookModal.style.display = "none";
+});
+
+imageFile.addEventListener("change", function () {
+  const currentImage = imageFile.files[0];
+  if (currentImage) {
+    fileChosen.style.display = "block";
+    fileChosen.src = URL.createObjectURL(currentImage);
+  }
 });
